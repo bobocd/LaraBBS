a7234ef2d36ea16bd6506b86c2f345c4ed9ac538
bobocd <twocars@163.com>
2020-08-30 17:19:23 +0800

    认证后的消息提醒

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,7 +14,7 @@
 <div id="app" class="root-page">
   @include('layouts._header')
   <div class="container">
-    {{--@include('shared._messages')--}}
+    @include('shared._messages')
     @yield('content')
   </div>
   @include('layouts._footer')
