38af6f2731c6a28eb95166d59897c1a19ce28183
bobocd <twocars@163.com>
2020-08-31 22:47:46 +0800

    授权访问

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -9,7 +9,10 @@ use App\Handlers\ImageUploadHandler;
 
 class UsersController extends Controller
 {
-
+    public function __construct()
+    {
+        $this->middleware('auth', ['except' => ['show']]);
+    }
     /**
      * Display the specified resource.
      *
@@ -29,7 +32,7 @@ class UsersController extends Controller
      */
     public function edit(User $user)
     {
-//        $this->authorize('update', $user);
+        $this->authorize('update', $user);
         return view('users.edit', compact('user'));
     }
 
@@ -42,6 +45,7 @@ class UsersController extends Controller
      */
     public function update(UserRequest $request, ImageUploadHandler $uploader,User $user)
     {
+        $this->authorize('update', $user);
         $data = $request->all();
         if ($request->avatar) {
             $result = $uploader->save($request->avatar, 'avatars', $user->id, 416);
