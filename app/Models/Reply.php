0c634ac5afcd404db65dad5d34ece0a7fc9c8a43
bobocd <twocars@163.com>
2020-09-12 12:15:44 +0800

    回复列表

--- app/Models/Reply.php
+++ app/Models/Reply.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace App\Models;
-
+use App\User;
 
 class Reply extends Model
 {
--- app/User.php
+++ app/User.php
@@ -6,6 +6,7 @@ use Illuminate\Notifications\Notifiable;
 use Illuminate\Auth\MustVerifyEmail as MustVerifyEmailTrait;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Contracts\Auth\MustVerifyEmail as MustVerifyEmailContract;
+use App\Models\Reply;
 
 class User extends Authenticatable implements MustVerifyEmailContract
 {
