b6575eceb91f35a79a4e7cb0edc34a75885f1b1a
bobocd <twocars@163.com>
2020-09-15 21:32:52 +0800

    消息通知有新回复

--- app/Observers/TopicObserver.php
+++ app/Observers/TopicObserver.php
@@ -5,6 +5,7 @@ namespace App\Observers;
 use App\Models\Topic;
 use App\Handlers\SlugTranslateHandler;
 use App\Jobs\TranslateSlug;
+use App\Notifications\TopicReplied;
 
 // creating, created, updating, updated, saving,
 // saved,  deleting, deleted, restoring, restored
@@ -29,10 +30,6 @@ class TopicObserver
             // 推送任务到队列
             dispatch(new TranslateSlug($topic));
         }
-    }
-    public function creating(Topic $topic)
-    {
-        //
     }
 
     public function updating(Topic $topic)
