069cc1afd77c10b4f648915950f6ad25bbca4360
bobocd <twocars@163.com>
2020-12-20 23:45:36 +0800

    add captcha

--- app/Http/Requests/Api/VerificationCodeRequest.php
+++ app/Http/Requests/Api/VerificationCodeRequest.php
@@ -23,12 +23,22 @@ class VerificationCodeRequest extends FormRequest
      */
     public function rules()
     {
+//        return [
+//            'phone' => [
+//                'required',
+//                'regex:/^1(3[0-9]|5[0-3,5-9]|7[1-3,5-8]|8[0-9])\d{8}$/',
+//                'unique:users'
+//            ]
+//        ];
         return [
-            'phone' => [
-                'required',
-                'regex:/^1(3[0-9]|5[0-3,5-9]|7[1-3,5-8]|8[0-9])\d{8}$/',
-                'unique:users'
-            ]
+            'captcha_key' => 'required|string',
+            'captcha_code' => 'required|string',
+        ];
+    }
+    public function attributes(){
+        return [
+            'captcha_key' => '图片验证码 key',
+            'captcha_code' => '图片验证码',
         ];
     }
 }
